81723ac94372397f21a4c344f28508e52b0efd0b
Matt Styles <matt@personalurban.com>
2024-01-10 18:11:38 +0000

    add nextjs stylex example

---
+++ nextjs-stylex-import/next.config.mjs
@@ -0,0 +1,29 @@
+
+import path from 'node:path'
+import {fileURLToPath} from 'node:url'
+
+// const stylexPlugin = require('@stylexjs/nextjs-plugin');
+import stylexPlugin from '@stylexjs/nextjs-plugin'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: [
+    'stylex-component-export'
+  ],
+  webpack(config) {
+    config.resolve.extensionAlias = {
+      '.js': ['.js', '.ts'],
+      '.jsx': ['.jsx', '.tsx'],
+    }
+    return config
+  },
+}
+
+
+
+export default stylexPlugin({
+  rootDir: __dirname,
+})(nextConfig);
---
+++ nextjs-stylex-import/src/app/page.tsx
@@ -0,0 +1,25 @@
+
+import stylex from '@stylexjs/stylex'
+import {Test} from 'stylex-component-export'
+
+const styles = stylex.create({
+  main: {
+    padding: 8
+  },
+  container: {
+    backgroundColor: 'rebeccapurple',
+    padding: 8,
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 700
+  }
+})
+
+export default function Home() {
+  return (
+    <main className={stylex(styles.main)}>
+      <div className={stylex(styles.container)}>Hello world from stylex</div>
+      <Test>Hello world from external library</Test>
+    </main>
+  )
+}
